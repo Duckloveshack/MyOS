3902393c218accabb94928f5f212fb5dc17e1a30
Duckloveshack <54633619+Duckloveshack@users.noreply.github.com>
2019-12-07 22:29:45 +0200

    MyOS - Orange JailBreak for MyOS
    A new Jailbreak method for MyOS 1.4.1 or higher

---
+++ OrangeInstaller.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo.
+echo ------------------------------------------------------------------------------------------------------------------------
+echo.
+echo 						     Orange JailBreak
+echo 						      For MyOS 1.4.1
+echo.
+echo ------------------------------------------------------------------------------------------------------------------------
+timeout /T 3 /NOBREAK >nul
+echo Running exploit...
+timeout /T 10 /NOBREAK >nul
+echo Follow the instructions shown in MyOS.
+echo Press [ENTER] to exit
+pause >nul
+exit
