c7422d3f8e01411af496426873f56157c5f84fc7
Duckloveshack <54633619+Duckloveshack@users.noreply.github.com>
2019-12-26 10:02:10 +0200

    Pos1xeye Jailbreak (MyOS 1.7.0)
    Pos1xeye - the best global jailbreak for MyOS 1.7.0

---
+++ pos1xeye.cmd
@@ -0,0 +1,13 @@
+@echo off
+timeout /T 1 /NOBREAK >nul
+title POS1XEYE
+echo Pos1xeye for MyOS 1.7.0
+echo.
+echo The best global jailbreak for MyOS 1.7.0
+timeout /T 1 /NOBREAK >nul
+echo.
+echo You may close this window and continue the operation using MyOS.
+echo.
+echo Press [ENTER] to Exit
+pause >nul
+exit
