c723b2cf07edf343ec7ca0781eca0da98534c2d6
Duckloveshack <54633619+Duckloveshack@users.noreply.github.com>
2019-12-30 13:40:32 +0200

    Pos1xeye Jailbreak for MyOS 1.7.0-1.8.0

--- pos1xeye.cmd
+++ pos1xeye.cmd
@@ -1,12 +1,43 @@
 @echo off
 timeout /T 1 /NOBREAK >nul
 title POS1XEYE
-echo Pos1xeye for MyOS 1.7.0
+echo PPPPPPPPPPPPPPPPP       OOOOOOOOO       SSSSSSSSSSSSSSS  1111111  XXXXXXX       XXXXXXX
+echo P::::::::::::::::P    OO:::::::::OO   SS:::::::::::::::S1::::::1  X:::::X       X:::::X
+echo P::::::PPPPPP:::::P OO:::::::::::::OOS:::::SSSSSS::::::1:::::::1  X:::::X       X:::::X
+echo PP:::::P     P:::::O:::::::OOO:::::::S:::::S     SSSSSS111:::::1  X::::::X     X::::::X
+echo   P::::P     P:::::O::::::O   O::::::S:::::S              1::::1  XXX:::::X   X:::::XXX
+echo   P::::P     P:::::O:::::O     O:::::S:::::S              1::::1     X:::::X X:::::X
+echo   P::::PPPPPP:::::PO:::::O     O:::::OS::::SSSS           1::::1      X:::::X:::::X  
+echo   P:::::::::::::PP O:::::O     O:::::O SS::::::SSSSS      1::::l       X:::::::::X
+echo   P::::PPPPPPPPP   O:::::O     O:::::O   SSS::::::::SS    1::::l       X:::::::::X
+echo   P::::P           O:::::O     O:::::O      SSSSSS::::S   1::::l      X:::::X:::::X
+echo   P::::P           O:::::O     O:::::O           S:::::S  1::::l     X:::::X X:::::X
+echo   P::::P           O::::::O   O::::::O           S:::::S  1::::l  XXX:::::X   X:::::XXX
+echo PP::::::PP         O:::::::OOO:::::::SSSSSSS     S:::::111::::::11X::::::X     X::::::X
+echo P::::::::P          OO:::::::::::::OOS::::::SSSSSS:::::1::::::::::X:::::X       X:::::X
+echo P::::::::P            OO:::::::::OO  S:::::::::::::::SS1::::::::::X:::::X       X:::::X
+echo PPPPPPPPPP              OOOOOOOOO     SSSSSSSSSSSSSSS  11111111111XXXXXXX       XXXXXXX
 echo.
-echo The best global jailbreak for MyOS 1.7.0
+echo.
+echo EEEEEEEEEEEEEEEEEEEEEYYYYYYY       YYYYYYEEEEEEEEEEEEEEEEEEEEEE
+echo E::::::::::::::::::::Y:::::Y       Y:::::E::::::::::::::::::::E
+echo E::::::::::::::::::::Y:::::Y       Y:::::E::::::::::::::::::::E
+echo EE::::::EEEEEEEEE::::Y::::::Y     Y::::::EE::::::EEEEEEEEE::::E
+echo   E:::::E       EEEEEYYY:::::Y   Y:::::YYY E:::::E       EEEEEE
+echo   E:::::E               Y:::::Y Y:::::Y    E:::::E             
+echo   E::::::EEEEEEEEEE      Y:::::Y:::::Y     E::::::EEEEEEEEEE   
+echo   E:::::::::::::::E       Y:::::::::Y      E:::::::::::::::E   
+echo   E:::::::::::::::E        Y:::::::Y       E:::::::::::::::E   
+echo   E::::::EEEEEEEEEE         Y:::::Y        E::::::EEEEEEEEEE   
+echo   E:::::E                   Y:::::Y        E:::::E             
+echo   E:::::E       EEEEEE      Y:::::Y        E:::::E       EEEEEE
+echo EE::::::EEEEEEEE:::::E      Y:::::Y      EE::::::EEEEEEEE:::::E
+echo E::::::::::::::::::::E   YYYY:::::YYYY   E::::::::::::::::::::E
+echo E::::::::::::::::::::E   Y:::::::::::Y   E::::::::::::::::::::E
+echo EEEEEEEEEEEEEEEEEEEEEE   YYYYYYYYYYYYY   EEEEEEEEEEEEEEEEEEEEEE
 timeout /T 1 /NOBREAK >nul
 echo.
-echo You may close this window and continue the operation using MyOS.
+echo Please choose an option in MyOS.
 echo.
 echo Press [ENTER] to Exit
 pause >nul
